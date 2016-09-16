141d73e4184bb69f6226472de6365e5434dfdf06
Chad Coleman <cjc2238@tc.columbia.edu>
2016-09-16 11:37:22 -0400

    Converting string to factors.
    Selecting subsets of data based on specified value.
    
    Assignign subset of data to new data frame.

--- Used Cars.R
+++ Used Cars.R
@@ -11,3 +11,11 @@ df4 <- read.csv("usedcars.csv", stringsAsFactors = FALSE)
 # compare the data frames
 str(df3)
 str(df4)
+# convert transmission to factor 
+df4$transmission <- factor(df4$transmission)
+# rename df4 to df
+df <- df4
+# Take a subset of the data (df) and only select the yellow cars.
+subset(df, color %in% "Yellow")
+# assign yellow cars subset of data frame
+yellow_cars <- subset(df, color %in% "Yellow")
