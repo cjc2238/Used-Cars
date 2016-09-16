cf9c12227749ad08520c989ce6583e3992154a54
Chad Coleman <cjc2238@tc.columbia.edu>
2016-09-16 10:45:21 -0400

    inspect data frame structure

--- Used Cars.R
+++ Used Cars.R
@@ -1,3 +1,5 @@
 # Load CSV
 # Set all strings to not be factors in order to manueally create the factors later.
 df3 <- read.csv("usedcars.csv", stringsAsFactors = FALSE)
+# Inspect data frame structure
+str(df3)
